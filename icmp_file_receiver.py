834c75cb014ec93f717fae5597b2310ed282b2a5
dashboardtobie <132006223+dashboardtobie@users.noreply.github.com>
2024-11-30 23:02:10 +0100

    Add files via upload

---
+++ icmp_file_receiver.py
@@ -0,0 +1,47 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Directory listing for /</title>
+</head>
+<body>
+<h1>Directory listing for /</h1>
+<hr>
+<ul>
+<li><a href="arp_mitm.pcapng">arp_mitm.pcapng</a></li>
+<li><a href="arp_mitm.py">arp_mitm.py</a></li>
+<li><a href="arp_poisonning.py">arp_poisonning.py</a></li>
+<li><a href="arp_spoof.pcapng">arp_spoof.pcapng</a></li>
+<li><a href="bin/">bin/</a></li>
+<li><a href="capture_stp.py">capture_stp.py</a></li>
+<li><a href="dernier.py">dernier.py</a></li>
+<li><a href="dhcp_starv.py">dhcp_starv.py</a></li>
+<li><a href="dhcp_starvation.pcapng">dhcp_starvation.pcapng</a></li>
+<li><a href="dhcp_starvation.py">dhcp_starvation.py</a></li>
+<li><a href="dns_cap.py">dns_cap.py</a></li>
+<li><a href="dns_lookup.py">dns_lookup.py</a></li>
+<li><a href="dns_spoof.py">dns_spoof.py</a></li>
+<li><a href="icmp_basic_exfiltr.py">icmp_basic_exfiltr.py</a></li>
+<li><a href="icmp_basic_receiver.py">icmp_basic_receiver.py</a></li>
+<li><a href="icmp_file_exfilter.py">icmp_file_exfilter.py</a></li>
+<li><a href="icmp_file_exfiltration.pcapng">icmp_file_exfiltration.pcapng</a></li>
+<li><a href="icmp_file_receiver.py">icmp_file_receiver.py</a></li>
+<li><a href="include/">include/</a></li>
+<li><a href="lib/">lib/</a></li>
+<li><a href="lib64/">lib64@</a></li>
+<li><a href="mac_add.py">mac_add.py</a></li>
+<li><a href="ping.py">ping.py</a></li>
+<li><a href="pyvenv.cfg">pyvenv.cfg</a></li>
+<li><a href="share/">share/</a></li>
+<li><a href="stp.py">stp.py</a></li>
+<li><a href="stp_rb.py">stp_rb.py</a></li>
+<li><a href="tcp_cap.py">tcp_cap.py</a></li>
+<li><a href="tcp_hijack.py">tcp_hijack.py</a></li>
+<li><a href="tcp_rst.py">tcp_rst.py</a></li>
+<li><a href="test.py">test.py</a></li>
+<li><a href="test2.py">test2.py</a></li>
+<li><a href="yo.py">yo.py</a></li>
+</ul>
+<hr>
+</body>
+</html>
